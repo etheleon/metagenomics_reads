fae0fad5090598ef0434a8004deacfb83f7aed68
etheleon <picy2k@gmail.com>
2014-02-10 23:11:21 +0800

    Redone, now its complete

--- seq.0252.familyORnot.pl
+++ seq.0252.familyORnot.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl 
 
+die "Usage: seq.0252.familyORnot.pl input output" unless $#ARGV == 1;
+
 use lib "/export2/home/uesu/perl5/lib/perl5";
 use strict;
 use REST::Neo4p;
@@ -8,40 +10,41 @@ use REST::Neo4p::Query;
 my $server = 'http://192.168.100.1:7474'; 
 REST::Neo4p->connect($server);
 
+open(INPUT, $ARGV[0]) 		|| die $!;
+open(OUTPUT, ">$ARGV[1]") 	|| die $!;
 
-#Problem with script now is that the genus may actually lead to a blank; ie. it 
-open(INPUT, $ARGV[0]) || die $!;
-open(OUTPUT, ">$ARGV[1]") || die $!;
+#Step 1a: Stores the genera into an %genera=$family
+my %genera =();	#storing the genus -> family hash
+while(<INPUT>) { my @F=split("\t",$_); my @a=split("_",$F[1]); foreach(@a) { $genera{$_}=() }	}
 
+#Step 1b: Find the family id for that genus
 my $stmt='start basetaxa=node:ncbitaxid(taxid={taxids}) optional match basetaxa-[:childof*]->(family:`family`) return family.taxid';
-while(<INPUT>) { 
-    chomp;
-    if ($. == 1){
-	print OUTPUT "$_"."\tfamilyLCAable\n"
-    }else{
-    	print OUTPUT $_;	#prints the existing line
-    	    my @F = split("\t");
-#K00012  120831_146918   2       0
-	my @glos=split("_",$F[1]); 		#pushing the glos into the array @glos
-	    my %fam=();
-	foreach my $basetaxa (@glos){		#for each of the genera find the family
-	    my $query = REST::Neo4p::Query->new($stmt, {taxids=>$basetaxa}); 
-	    print $basetaxa,"\n";
+foreach my $genus (keys %genera) { 
+	    my $query = REST::Neo4p::Query->new($stmt, {taxids=>$genus}); 
 	    $query->execute;	
-#add the family taxid to the hash %fam
-    	    while(my $result = $query->fetch){
-    	    	if($result->[0] eq ''){
-    	    	    $fam{'NULL'}=();
+    	    while(my $family = $query->fetch){
+    	    	if($family->[0] eq ''){
+    	    	    $genera{$genus} = 'NULL';	#id if that genus doesnt have a family
     	    	}else{
-    	    	    my $family=$result->[0];	
-    	    	    $fam{$family}=();
+    	    	    $genera{$genus} = $family->[0];	
     	    	}
 	    }
+}
+
+
+#Go back to the start
+seek INPUT, 0, 0;
+while(<INPUT>){ 
+    chomp;
+    if (/^ko/){print OUTPUT "$_"."\tfamilyLCAable\n"; 	#header
+    }else{
+	#K00012  120831_146918   2       0
+    	my @F = split("\t");
+	my @glos=split("_",$F[1]); 		#pushing the glos into the array @glos
+	my %flo=();
+	foreach(@glos){ 
+	$flo{$genera{$_}}=();
 	}
-	my $i=0;
-	foreach(keys %fam){
-	    $i++;
-	}
-	print OUTPUT "\t$i\n";
+	print OUTPUT "$_\t", scalar keys %flo,"\n";
     }
 }
